7129de191f78bb125f2077e8f1d0189c48dbcf70
Muhammad B <85152396+MuhammadDev-OP@users.noreply.github.com>
2022-10-03 19:50:22 +0500

    Update Task#31.js

--- Task#31.js
+++ Task#31.js
@@ -14,7 +14,7 @@ var new_users = [
 ];
 
 for(let i=0 ; i < new_users.length; i++){
-    if( current_users[i] = new_users[i].toLowerCase() ){
+    if( current_users[i] == new_users[i].toLowerCase() ){
         console.log("Sorry " + new_users[i] + ", that name is taken.");
     }
     else{
